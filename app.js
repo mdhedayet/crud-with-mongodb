51f904abbff2ab73b41401e79779a08fdbf72bd4
Md Hedayet Ullah <tanmoy15-1004@diu.edu.bd>
2023-01-16 12:35:15 +0600

    mongoose set error solved

--- app.js
+++ app.js
@@ -4,28 +4,21 @@ const blogRouter = require("./routes/BlogRoutes");
 
 const app = express();
 
+const mongooseUri = "mongodb://localhost:27017/CRUD";
+const PORT = 3001;
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/blogs", blogRouter);
 
-//configure mongoose
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected to MongoDB");
-    }
-  }
-);
 
-app.listen(3001, () => {
+mongoose.set("strictQuery", false);
+mongoose.connect(mongooseUri, () => {
+  console.log("Connected to MongoDB");
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
